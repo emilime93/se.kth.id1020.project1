2e2f4209eacaa34cbd9f1c297cb867188f110217
Emil Lindholm Brandt <emil93@gmail.com>
2017-09-30 11:54:32 +0200

    Big commit. Add and search functions are now implemented. The add is binary but the search is still linear, that's what I'm about to fix!

--- src/main/java/se/kth/emibra/search_engine/TinySearchEngine.java
+++ src/main/java/se/kth/emibra/search_engine/TinySearchEngine.java
@@ -14,34 +14,55 @@ public class TinySearchEngine implements TinySearchEngineBase {
 
     public void insert(Word word, Attributes attributes) {
         WordAttrTuple wat = new WordAttrTuple(word, attributes);
-//        index.add(wat);
-//        addToIndex(wat);
-        addToIndexBinary(wat, 0, index.size()/2, index.size()-1);
+        int i = indexOf(wat.getWord().word);
+        index.add(i, wat);
     }
 
-    private void addToIndexBinary(WordAttrTuple wat, int lo, int mid, int hi) {
-        if (index.size() == 0) {
-            index.add(wat);
-            return;
+    private int indexOf(String word) {
+        int lo = 0;
+        int hi = index.size() - 1;
+        if (index.size() == 0)
+            return 0;
+        int mid = 0;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if (index.get(mid).getWord().word.compareTo(word) > 0) {    // The target word is in the left half
+                hi = mid - 1;
+            } else if (index.get(mid).getWord().word.compareTo(word) < 0) {     // The target word is in the left half
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
         }
-        if (lo == (hi+1)) {
-            //We haven't found the word and it is to be inserted between the indices
-            index.add(lo, wat);
-        } else if (true) {
-
-        } else if (index.get(mid).getWord().word.compareTo(wat.getWord().word) == 0) {
-            //New word occurande
-            Attributes atr = wat.getAttributes().get(0);
-            index.get(mid).addOccurrence(atr);
-            return;
-        } else if(index.get(mid).getWord().word.compareTo(wat.getWord().word) < 0) {
-            // GO RIGHT
-            addToIndexBinary(wat, mid, (hi-mid)/2, hi);
-        } else if (index.get(mid).getWord().word.compareTo(wat.getWord().word) > 0) {
-            // GO LEFT
-            addToIndexBinary(wat, lo, (mid-lo)/2, mid);
-        }
+        return mid;
     }
+
+//    private void addToIndexBinary(WordAttrTuple wat) {
+//        int lo = 0, hi = index.size()-1;
+//        String word = wat.getWord().word;
+//
+//
+//        int mid;
+//        while (lo <= hi) {
+//            System.out.printf("Size: %d\n", index.size());
+//            mid = lo + (hi - lo) / 2;
+//            if (index.get(mid).getWord().word.compareTo( word ) > 0) // The word is in the left half
+//                hi = mid - 1;
+//            else if (index.get(mid).getWord().word.compareTo( word ) < 0) // The word is in the right half
+//                lo = hi + 1;
+//            else if (index.get(mid).getWord().word.compareTo( word ) == 0) // The word is found, occurance added
+//            {
+//                System.out.printf("Adding word occurance %s @ %d\n", index.get(mid).getWord().word, mid);
+//                index.get(mid).addOccurrence(wat.getAttributes().get(0));
+//                return;
+//            }
+//            else {
+//                System.out.printf("Adding %s\n", index.get(mid).getWord().word);
+//                index.add(mid, wat);
+//                return;
+//            }
+//        }
+//    }
 
     private void addToIndex(WordAttrTuple wat) {
         for (int i = 0; i < index.size()-1; i++) {
@@ -53,12 +74,44 @@ public class TinySearchEngine implements TinySearchEngineBase {
         index.add(wat);
     }
 
-    public List<Document> search(String s) {
-        List<Document> list = new ArrayList<Document>();
-        for (WordAttrTuple wat :
-                index) {
-//            list.add(wat.getAttributes().);
+    /*public List<Document> search(String word) {
+        int lo = 0;
+        int hi = index.size() - 1;
+        int mid = 0;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+            if (index.get(mid).getWord().word.compareTo(word) > 0) {
+                hi = mid - 1;
+            } else if (index.get(mid).getWord().word.compareTo(word) < 0) {
+                lo = mid + 1;
+            } else {
+                break;
+            }
         }
-        return null;
+
+        String targetString = index.get(mid).getWord().word;
+        String s = targetString;
+        while (s.equalsIgnoreCase(index.get(mid-1).getWord().word) && mid >= 0) {
+            s = index.get(mid).getWord().word;
+            mid--;
+        }
+
+        List<Document> list  = new ArrayList<Document>();
+        while (targetString.equalsIgnoreCase(index.get(mid).getWord().word)) {
+            list.add(index.get(mid++).getAttributes().get(0).document);
+        }
+
+        return list;
+    }*/
+
+    public List<Document> search(String s) {
+        System.out.println("Searching for " + s);
+        List<Document> list = new ArrayList<Document>();
+        for (WordAttrTuple wat : index) {
+            if (wat.getWord().word.equalsIgnoreCase(s)) {
+                list.add(wat.getAttributes().get(0).document);
+            }
+        }
+        return list;
     }
 }
